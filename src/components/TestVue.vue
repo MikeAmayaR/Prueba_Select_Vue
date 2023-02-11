39d4a2d15074ea3720f084cd68a0647eaa3d2820
Mike Amaya <michael@lizzapp.com>
2023-02-11 12:26:34 -0500

    Create select with vue.js

---
+++ src/components/TestVue.vue
@@ -0,0 +1,26 @@
+<template>
+    <select name="" id="" v-model="products" v-for="reponses in products" >
+        <option :value="reponses">{{ reponses }}</option>
+    </select>
+</template>
+
+<script lang="ts">
+import axios from "axios";
+const baseUrl = import.meta.env.VITE_APP_ENV_FRUTS_URL;
+console.log('baseUrl', baseUrl)
+export default {
+    data() {
+        return { products: {} };
+    },
+    mounted() {
+        this.getProducts();
+    },
+    methods: {
+        getProducts() {
+            axios
+                .get(`${baseUrl}`)
+                .then((response) => (this.products = response.data));
+        },
+    },
+};
+</script>
