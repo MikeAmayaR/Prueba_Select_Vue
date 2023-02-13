b3d381b2fce03c4681361302e6a554f1625f8f76
Mike Amaya <michael@lizzapp.com>
2023-02-12 21:43:33 -0500

    finish design select test

--- src/components/TestVue.vue
+++ src/components/TestVue.vue
@@ -1,16 +1,24 @@
 <template>
-    <select name="" id="" v-model="products" v-for="reponses in products" >
-        <option :value="reponses">{{ reponses }}</option>
-    </select>
+    <div>
+        <select name="select" class="select" v-for="data in products">
+            <option
+                class="new-color"
+                :value="data"
+                v-for="dataFinish in data.fruits"
+            >
+                {{ dataFinish }}
+            </option>
+        </select>
+    </div>
 </template>
 
 <script lang="ts">
 import axios from "axios";
+
 const baseUrl = import.meta.env.VITE_APP_ENV_FRUTS_URL;
-console.log('baseUrl', baseUrl)
 export default {
     data() {
-        return { products: {} };
+        return { products: null };
     },
     mounted() {
         this.getProducts();
@@ -24,3 +32,30 @@ export default {
     },
 };
 </script>
+
+<style>
+select {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    padding: 16px;
+    gap: 85px;
+    border-color: #f3f3f3;
+    position: absolute;
+    width: 256px;
+    height: 56px;
+    left: 32px;
+    top: 32px;
+    background: #ffffff;
+    box-shadow: 0px 10px 15px rgba(35, 78, 82, 0.1);
+    border-radius: 8px;
+}
+
+.select {
+    color: grey;
+}
+
+.select option {
+    color: black;
+}
+</style>
